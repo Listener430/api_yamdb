d2f0d8a914bb4d6ab9632ce26fdf6fdc99d83373
Надежда Калмыкова <nklepik@mail.ru>
2022-04-13 16:02:10 +0300

    feature/DZ-1-add-serializers

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -32,12 +32,16 @@ class CategoryViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name",)
 
 
 class GenreViewSet(viewsets.ModelViewSet):
     queryset = Genre.objects.all()
     serializer_class = GenreSerializer
     permission_classes = (IsAdminOrReadOnly,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ("name",)
 
 
 class TitleViewSet(viewsets.ModelViewSet):
