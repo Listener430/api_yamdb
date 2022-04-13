ff69eaa3dbf1b3e221e632bb2acaa60bda35e1d9
Алексей Карпов <a01alex@yandex.ru>
2022-04-13 14:13:02 +0300

    допилил авторизацию добавил API_USER

--- api_yamdb/users/views.py
+++ api_yamdb/users/views.py
@@ -3,9 +3,9 @@ from django.core.mail import send_mail
 from django.shortcuts import get_object_or_404
 
 
-from rest_framework import viewsets, filters, status
-from rest_framework.decorators import action, api_view, permission_classes
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken
 
